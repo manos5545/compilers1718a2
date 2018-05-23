799f52de05f68826440cebee13e246b9cbfb71a5
manos5545 <manos5545@yahoo.gr>
2018-05-23 11:02:28 +0300

    Added Glossary

--- parser.py
+++ parser.py
@@ -1,3 +1,16 @@
+'''
+Glossary
+
+Stmt_list       ->      Stmt Stmt_list | ε
+Stmt            ->      id = Expr | print Expr
+Expr            ->      Term Term_tail
+Term_tail       ->      Logic Term Term_tail | ε
+Term            ->      Factor Factor_tail
+Factor_tail     ->      Parenthesis Factor Factor_tail | ε
+Factor          ->      (Expr) | id | true | false | t | f | 1 | 0
+Logic           ->      not | and | or
+Parenthesis     ->      ( | )
+'''
 
 import plex
 
